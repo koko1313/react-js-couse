5394383dba34468deb47c9446b23ab21e5997e81
koko1313 <kaloyanvellichkov@gmail.com>
2020-01-11 14:41:28 +0200

    quantity input styling

--- src/components/quantityInput/index.js
+++ src/components/quantityInput/index.js
@@ -12,11 +12,11 @@ class QuantityInput extends Component {
     render() {
         return <div className="input-group my-2">
             <div className="input-group-prepend">
-                <button className="btn btn-outline-secondary" type="button">Button</button>
+                <button className="btn btn-outline-success" type="button"><i className="fa fa-minus"></i></button>
             </div>
-            <input type="text" className="form-control"/>
+            <input type="text" className="form-control text-center" value={this.state.quantity}/>
             <div className="input-group-append">
-                <button className="btn btn-outline-secondary" type="button">Button</button>
+                <button className="btn btn-outline-success" type="button"><i className="fa fa-plus"></i></button>
             </div>
         </div>
     }
